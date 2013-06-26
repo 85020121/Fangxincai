20cea139e45b37c2a61630998ecd11805565678b
Bowen <bowengao3@gmail.com>
2013-06-26 08:17:39 +0200

    sfr server

--- fangxincai/FXCDefine.h
+++ fangxincai/FXCDefine.h
@@ -7,8 +7,8 @@
 //
 
 #define FXC_SERVER_HEAD                 @"http://bowen.gao.perso.sfr.fr"
-#define FXC_PRODUCTS_REQUEST            @"http://192.168.1.83:8888/protected/iOS/ios_tools.php?iosRequest=iosGetProducts"
-#define FXC_PRODUCTS_BY_IDS             @"http://192.168.1.83:8888/protected/iOS/ios_tools.php?iosRequest=iosGetProductsByIDs"
-#define FXC_PRODUCTS_BY_INTERVAL        @"http://192.168.1.83:8888/protected/iOS/ios_tools.php?iosRequest=iosGetProductsByInterval"
+#define FXC_PRODUCTS_REQUEST            @"http://bowen.gao.perso.sfr.fr/protected/iOS/ios_tools.php?iosRequest=iosGetProducts"
+#define FXC_PRODUCTS_BY_IDS             @"http://bowen.gao.perso.sfr.fr/protected/iOS/ios_tools.php?iosRequest=iosGetProductsByIDs"
+#define FXC_PRODUCTS_BY_INTERVAL        @"http://bowen.gao.perso.sfr.fr/protected/iOS/ios_tools.php?iosRequest=iosGetProductsByInterval"
 #define REQUEST_PRODUCTS_INTERVAL       15
 
